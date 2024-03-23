aef655617d21da2b6fd3f36f2bcd791705ca9ce2
cheryl9012 <161365360+cheryl9012@users.noreply.github.com>
2024-03-23 21:49:03 +0500

    Add files via upload

---
+++ Q34.js
@@ -0,0 +1,6 @@
+"use strict";
+let pizzas = ["Margherita", " chicken tikka", " Fajita"];
+pizzas.forEach(pizzas => {
+    console.log(`I love to eat ${pizzas} pizza flavour.`);
+});
+console.log(`I love to eat pizza.`);
---
+++ Q34.ts
@@ -0,0 +1,9 @@
+
+
+
+let pizzas : string[]= ["Margherita", " chicken tikka", " Fajita"];
+
+pizzas.forEach(pizzas => {
+    console.log( `I love to eat ${pizzas} pizza flavour.`);
+});
+console.log(`I love to eat pizza.`);
